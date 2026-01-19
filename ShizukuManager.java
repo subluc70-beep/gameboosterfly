38f02145ec5e80ca1e6178807c93a936f31772ef
subluc70-beep <subluc70@gmail.com>
2026-01-18 21:20:16 -0300

    Update ShizukuManager.java

--- ShizukuManager.java
+++ ShizukuManager.java
@@ -1,1 +1,23 @@
+package com.seunome.gamebooster;
 
+import dev.rikka.shizuku.Shizuku;
+import android.content.pm.PackageManager;
+
+public class ShizukuManager {
+
+    public static boolean temPermissao() {
+        if (Shizuku.pingBinder()) {
+            return Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    public static void executarComando(String comando) {
+        try {
+            // Execução via Binder Nativo (Mais rápido que Risu)
+            Shizuku.newProcess(new String[]{"sh", "-c", comando}, null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
