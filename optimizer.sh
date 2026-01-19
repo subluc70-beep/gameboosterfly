944b33f3a206220d3a0222988751bcfaff8040b3
subluc70-beep <subluc70@gmail.com>
2026-01-18 21:22:34 -0300

    Update optimizer.sh

--- optimizer.sh
+++ optimizer.sh
@@ -1,1 +1,15 @@
+#!/system/bin/sh
 
+# 1. Prioridade Máxima de CPU
+for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
+    echo "performance" > $cpu
+done
+
+# 2. Prioridade de Renderização GPU
+setprop debug.hwui.overdraw false
+setprop persist.sys.ui.hw true
+
+# 3. FPS Unlocker (Tenta forçar o buffer)
+setprop debug.gr.num_framebuffer_surface 3
+
+echo "Loki Booster: CPU e GPU em modo Máximo!"
